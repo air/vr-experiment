5dcc3426ac6219d79d8e54e54ca1f6070ac92588
Aaron Bell <aaron.bell@gmail.com>
2022-02-26 13:15:05 -0500

    init and render loop

--- js/main.js
+++ js/main.js
@@ -1,24 +1,33 @@
+// VR support
 import { VRButton } from '/lib/VRButton.js';
+// import { BoxLineGeometry } from '/lib/BoxLineGeometry.js';
+// import { XRControllerModelFactory } from '/lib/XRControllerModelFactory.js';
 
 const three = new Threestrap.Bootstrap();
-
 // export refs to Chrome console
 window.three = three;
 
-// test scene
+// globals
 const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xff3333 }));
-three.scene.add(cube);
-three.camera.position.set(1, 1, 2);
-three.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-// VR button
-document.body.appendChild(VRButton.createButton(three.renderer));
-// "tell your instance of WebGLRenderer to enable XR rendering"
-three.renderer.xr.enabled = true;
-// can't use window.requestAnimationFrame any more so:
-three.renderer.setAnimationLoop( function () {
-	three.renderer.render(three.scene, three.camera);
-} );
+init();
+// kick off animation. can't use window.requestAnimationFrame so:
+three.renderer.setAnimationLoop(function () {
+  three.renderer.render(three.scene, three.camera);
+});
+
+function init()
+{
+  // test scene
+  three.scene.add(cube);
+  three.camera.position.set(1, 1, 2);
+  three.camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+  // VR button
+  document.body.appendChild(VRButton.createButton(three.renderer));
+  // "tell your instance of WebGLRenderer to enable XR rendering"
+  three.renderer.xr.enabled = true;
+}
 
 three.on('update', function () {
   cube.rotateY(0.02);
